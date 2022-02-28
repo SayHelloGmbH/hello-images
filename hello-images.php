bcf4778109582d56c05a0d5ae78f8299af3218b8
Nico Martin <mail@nicomartin.ch>
2022-02-28 23:36:05 +0100

    updated version

--- hello-images.php
+++ hello-images.php
@@ -6,7 +6,7 @@ Plugin URI: https://github.com/SayHelloGmbH/Hello-Images
 Description: Image resize proxy for WordPress
 Author: Nico Martin
 Author URI: https://nico.dev
-Version: 0.1.8
+Version: 0.1.9
 Text Domain: hello-images
 Domain Path: /languages
  */
